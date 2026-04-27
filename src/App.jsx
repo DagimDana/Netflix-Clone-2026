a53d9a7ade4b3edb15cd9023787e43fabcd3e079
Dagim Dana <dagimdana1@gmail.com>
2026-04-27 20:47:26 +0300

    updated folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './App.css'
 import Header from './assets/Components/Header/Header'
 import Footer from './assets/Components/Footer/Footer'
-import Home from './assets/Pages/Home'
+import Home from './assets/Pages/Home/Home'
 import AdminPanel from './assets/Pages/AdminPage/AdminPanel'
 
 function App() {
